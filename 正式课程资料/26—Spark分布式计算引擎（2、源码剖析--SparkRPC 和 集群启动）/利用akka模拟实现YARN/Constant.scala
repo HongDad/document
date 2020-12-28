98434f07df14fbe05e10df25b1d8f49dee8b14e7
nice <naixuejiaoyunice@163.com>
2020-12-28 16:12:25 +0800

    no message

--- 正式课程资料/26—Spark分布式计算引擎（2、源码剖析--SparkRPC 和 集群启动）/利用akka模拟实现YARN/Constant.scala
+++
@@ -1,9 +0,0 @@
-package com.mazh.scala.core.day4.rpc.yarn01
-
-// TODO_MA 注释： 一些信息类
-object Constant {
-    val RMAS = "MyResourceManagerActorSystem"
-    val RMA = "MyResourceManagerActor"
-    val NMAS = "MyNodeManagerActorSystem"
-    val NMA = "MyNodeManagerActor"
-}
